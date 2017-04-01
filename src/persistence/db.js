f811afc2124ab6cad1b0ae597fca77927866b866
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-04-01 09:08:51 +0200

    :wrench: Integrate database functionality for businesses (#3)
    * :sparkles: Create database access layer for businesses
    
    * :white_check_mark: Test database connectivity
    
    * :white_check_mark: Test database functionality for businesses
    
    * :wrench: Improve database business modification test
    
    * :sparkles: Revamp database access layer testing

--- src/persistence/db.js
+++ src/persistence/db.js
@@ -9,11 +9,96 @@ class Database {
 
   connect() {
     return new Promise((resolve, reject) => {
-      MongoClient.connect(this.config.uri, (err, db) => {
-        if (err) reject(err);
-        this.db = db;
-        resolve(this);
-      });
+      MongoClient
+        .connect(this.config.uri, (err, db) => {
+          if (err) reject(err);
+          this.db = db;
+          resolve(this);
+        });
+    });
+  }
+
+  /** Businesses */
+
+  insertOneBusiness(business) {
+    return new Promise((resolve, reject) => {
+      this.db.collection('businesses')
+        .insertOne(business, ((err, data) => {
+          if (err) reject(err);
+          resolve(data.ops[0]);
+        }));
+    });
+  }
+
+  insertBusinesses(businesses) {
+    return new Promise((resolve, reject) => {
+      this.db.collection('businesses')
+        .insert(businesses, ((err, data) => {
+          if (err) reject(err);
+          resolve(data.ops);
+        }));
+    });
+  }
+
+  getAllBusinesses() {
+    return new Promise((resolve, reject) => {
+      this.db.collection('businesses')
+        .find({}).toArray((err, data) => {
+          if (err) reject(err);
+          resolve(data);
+        });
+    });
+  }
+
+  getBusinessesCount() {
+    return new Promise((resolve, reject) => {
+      this.db.collection('businesses')
+        .find({}).count((err, data) => {
+          if (err) reject(err);
+          resolve(data);
+        });
+    });
+  }
+
+  searchBusinesses(query) {
+    return new Promise((resolve, reject) => {
+      this.db.collection('businesses')
+        .find(query).toArray((err, data) => {
+          if (err) reject(err);
+          resolve(data);
+        });
+    });
+  }
+
+  getOneBusiness({ _id }) {
+    return new Promise((resolve, reject) => {
+      this.db.collection('businesses')
+        .findOne({ _id }, (err, data) => {
+          if (err) reject(err);
+          resolve(data);
+        });
+    });
+  }
+
+  modifyOneBusiness({ _id }, edits) {
+    return new Promise((resolve, reject) => {
+      this.db.collection('businesses')
+        .findAndModify({ _id }, [],
+        { $set: edits }, { new: true }, (err, data) => {
+          if (err) reject(err);
+          resolve(data.value);
+        });
+    });
+  }
+
+  deleteOneBusiness({ _id }) {
+    return new Promise((resolve, reject) => {
+      this.db.collection('businesses')
+        .findAndModify({ _id }, [],
+        {}, { remove: true }, (err, data) => {
+          if (err) reject(err);
+          resolve(data.value);
+        });
     });
   }
 
---
+++ test/tests.js
@@ -0,0 +1,95 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+const Database = require('../src/persistence/db');
+const config = require('../src/config/main.json');
+const businesses = require('./examples/businesses.json');
+
+const expect = chai.expect;
+chai.use(chaiAsPromised);
+
+const db = new Database(config.database);
+
+const beforeEachHook = () => db.drop();
+
+describe('Database', () => {
+
+  /** Initialization */
+
+  describe('Initialization', () => {
+    it('should connect', () => {
+      return expect(db.connect())
+        .to.eventually.not.be.empty;
+    });
+  });
+
+  /** Businesses */
+
+  describe('Businesses', () => {
+    beforeEach(beforeEachHook);
+
+    it('should insert one business', () => {
+      return Promise.resolve()
+        .then(() => expect(db.insertOneBusiness(businesses[0]))
+          .to.eventually.deep.equal(businesses[0]));
+    });
+
+    it('should insert all businesses', () => {
+      return Promise.resolve()
+        .then(() => expect(db.insertBusinesses(businesses))
+          .to.eventually.deep.equal(businesses));
+    });
+
+    it('should get all businesses', () => {
+      return Promise.resolve()
+        .then(() => db.insertBusinesses(businesses))
+        .then(() => expect(db.getAllBusinesses())
+          .to.eventually.deep.equal(businesses));
+    });
+
+    it('should count all businesses', () => {
+      return Promise.resolve()
+        .then(() => db.insertBusinesses(businesses))
+        .then(() => expect(db.getBusinessesCount())
+          .to.eventually.equal(businesses.length));
+    });
+
+    it('should search all businesses', () => {
+      return Promise.resolve()
+        .then(() => db.insertBusinesses(businesses))
+        .then(() => expect(db.searchBusinesses({ name: businesses[0].name }))
+          .to.eventually.deep.equal(businesses.filter(({ name }) => name === businesses[0].name)));
+    });
+
+    it('should get one business by _id', () => {
+      return Promise.resolve()
+        .then(() => db.insertBusinesses(businesses))
+        .then(([{ _id }]) => expect(db.getOneBusiness({ _id }))
+          .to.eventually.deep.equal(businesses[0]));
+    });
+
+    it('should modify one business by _id', () => {
+      return Promise.resolve()
+        .then(() => db.insertBusinesses(businesses))
+        .then(([{ _id }]) => db.modifyOneBusiness({ _id }, { name: 'Drumpf' }))
+        .then((data) => {
+          expect(data)
+            .to.have.property('_id')
+            .that.deep.equals(businesses[0]._id);
+          expect(data)
+            .to.have.property('name')
+            .that.not.deep.equals(businesses[0].name);
+          expect(data)
+            .to.have.property('description')
+            .that.deep.equals(businesses[0].description);
+        });
+    });
+
+    it('should delete one business by _id', () => {
+      return Promise.resolve()
+        .then(() => db.insertBusinesses(businesses))
+        .then(([{ _id }]) => expect(db.deleteOneBusiness({ _id }))
+          .to.eventually.deep.equal(businesses[0]));
+    });
+  });
+});
